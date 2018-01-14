63ebb69d5d95f27cb198d812f0dd7f3fb78898c9
Simon Pinkas <simon.pinkas@petzi.ch>
2018-01-14 16:38:50 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,12 @@
+var express = require('express')
+var app = express()
+
+app.listen(80, function() {
+    console.log('Chatfuel Bot-Server listening on port 80...');
+});
+
+app.get('/*', function(req, res) {
+    var jsonResponse = [];
+    jsonResponse.push({ "text": "Hi. " + (Math.random() * 5 + 1).toFixed(0) + " is a lucky number..." });
+    res.send(jsonResponse);
+});
