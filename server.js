21f69ef405d97a30079e0acad2d91ab22c9b72c1
Simon Pinkas <simon.pinkas@petzi.ch>
2018-01-14 16:43:29 +0100

    change port

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express')
 var app = express()
 
-app.listen(80, function() {
+app.listen(3000, function() {
     console.log('Chatfuel Bot-Server listening on port 80...');
 });
 
