d72f5c64e2913bdcc1a1ccce1fe092ca6c6c9f83
Simon Pinkas <simon.pinkas@petzi.ch>
2018-01-14 18:15:54 +0100

    remove emoji

--- routes/verses/single.js
+++ routes/verses/single.js
@@ -5,7 +5,7 @@ module.exports = (req, res) => {
 
   const verseId = Math.floor(Math.random() * data.sentences_tone.length);
   const verse = data.sentences_tone.find(m => m.sentence_id === verseId);
-  var response = "\:poop:" + verse.text;
+  var response = verse.text;
 
   jsonResponse.push({
 //    "text": "\:poop: " + response
