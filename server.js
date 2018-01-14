59b5310c61325b03453818caeadc728885292dd9
Simon Pinkas <simon.pinkas@petzi.ch>
2018-01-14 16:47:46 +0100

    Add name to workshop list

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 var express = require('express')
 var app = express()
 
-app.listen(3000, function() {
-    console.log('Chatfuel Bot-Server listening on port 80...');
+app.listen(8080, function() {
+    console.log('Chatfuel Bot-Server listening on port 8080');
 });
 
 app.get('/*', function(req, res) {
