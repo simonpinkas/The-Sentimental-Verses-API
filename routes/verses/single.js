233aba7ba8fe67d337ce8dbfce5a5d20b3644009
Simon Pinkas <simon.pinkas@petzi.ch>
2018-01-14 18:14:01 +0100

    more emoji tries

--- routes/verses/single.js
+++ routes/verses/single.js
@@ -5,10 +5,11 @@ module.exports = (req, res) => {
 
   const verseId = Math.floor(Math.random() * data.sentences_tone.length);
   const verse = data.sentences_tone.find(m => m.sentence_id === verseId);
-  var response = verse.text;
+  var response = "\:poop:" + verse.text;
 
   jsonResponse.push({
-    "text": "\:poop: " + response
+//    "text": "\:poop: " + response
+    "text": response
   });
 
   res.send(jsonResponse);
